481103d55ce57d31251f445a95495aa34408cb8e
Kimseungin0529 <whffkaos007@naver.com>
2024-01-16 17:14:47 +0900

    최초 커밋

---
+++ src/main/java/com/project/doongdoong/DoongdoongApplication.java
@@ -0,0 +1,13 @@
+package com.project.doongdoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoongdoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoongdoongApplication.class, args);
+	}
+
+}
