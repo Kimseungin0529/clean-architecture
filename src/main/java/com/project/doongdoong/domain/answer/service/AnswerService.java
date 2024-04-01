ac3a15b0f2497eedeb814b1ceee6c48a843ecdce
Kimseungin0529 <whffkaos007@naver.com>
2024-04-01 19:25:40 +0900

    feat : Answer 도메인 초기 설정

--- src/main/java/com/project/doongdoong/domain/answer/service/AnswerService.java
+++ src/main/java/com/project/doongdoong/domain/answer/service/AnswerService.java
@@ -1,2 +1,9 @@
-package com.project.doongdoong.domain.answer.service;public interface AnswerService {
+package com.project.doongdoong.domain.answer.service;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AnswerService {
+    public void createAnswer(MultipartFile multipartFile);
+
 }
--- src/main/java/com/project/doongdoong/domain/answer/service/AnswerServiceImp.java
+++ src/main/java/com/project/doongdoong/domain/answer/service/AnswerServiceImp.java
@@ -1,2 +1,20 @@
-package com.project.doongdoong.domain.answer.service;public class AnswerServiceImp {
+package com.project.doongdoong.domain.answer.service;
+
+import com.project.doongdoong.domain.answer.repository.AnswerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerServiceImp implements AnswerService{
+
+    private final AnswerRepository answerRepository;
+    private final WebClient webClient;
+
+    @Override
+    public void createAnswer(MultipartFile multipartFile) {
+        // mu
+    }
 }
