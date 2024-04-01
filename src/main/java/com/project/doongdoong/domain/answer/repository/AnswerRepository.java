d846469080f496a20125f9c10c890ee52ad749cb
Kimseungin0529 <whffkaos007@naver.com>
2024-04-01 19:24:48 +0900

    feat : Answer 도메인 초기 설정

---
+++ src/main/java/com/project/doongdoong/domain/answer/repository/AnswerRepository.java
@@ -0,0 +1,2 @@
+package com.project.doongdoong.domain.answer.repository;public interface AnswerRepository {
+}
---
+++ src/main/java/com/project/doongdoong/domain/answer/service/AnswerService.java
@@ -0,0 +1,2 @@
+package com.project.doongdoong.domain.answer.service;public interface AnswerService {
+}
