7dfb2f838828098a0afdfe3cbfdb6cf20df5d53d
Kimseungin0529 <whffkaos007@naver.com>
2025-06-26 13:36:43 +0900

    refactor : CounselStatisticsService 도입으로 인한 테스트 코드 변경

--- src/main/java/com/project/doongdoong/domain/counsel/Application.java
+++ src/main/java/com/project/doongdoong/domain/counsel/Application.java
@@ -2,10 +2,12 @@ package com.project.doongdoong.domain.counsel;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class Application implements CommandLineRunner {
     private final RedisRebuilder redisRebuilder;
 
